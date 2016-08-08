d064dc985990e875d90a733fbc7c94dba099435c
Aiur3908 <esobi@esobide-MacBook-Pro.local>
2016-08-08 18:39:29 +0800

    第一版

--- InfiniteCarousel/InfiniteCarousel/ViewController.h
+++ InfiniteCarousel/InfiniteCarousel/ViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
 @end
--- InfiniteCarousel/InfiniteCarousel/ViewController.m
+++ InfiniteCarousel/InfiniteCarousel/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //決定寬高等於螢幕寬，高度等於scrollView的高
+    CGFloat width, height;
+    width = self.view.frame.size.width;
+    height = self.scrollView.frame.size.height;
     
     //設定為翻頁模式
     [self.scrollView setPagingEnabled:YES];
@@ -23,11 +27,38 @@
     [self.scrollView setShowsVerticalScrollIndicator:NO];
     //隱藏水平scroll bar的顯示
     [self.scrollView setShowsHorizontalScrollIndicator:NO];
+    //設定ContentSize
+    [self.scrollView setContentSize:CGSizeMake(width * 3, height)];
+    //設定delegate
+    [self.scrollView setDelegate:self];
+    
+    //第一頁
+    UIView *page1View = [[UIView alloc]initWithFrame:CGRectMake(width * 0, 0, width, height)];
+    page1View.backgroundColor = [UIColor redColor];
+    [self.scrollView addSubview:page1View];
+    
+    //第二頁
+    UIView *page2View = [[UIView alloc]initWithFrame:CGRectMake(width * 1, 0, width, height)];
+    page2View.backgroundColor = [UIColor greenColor];
+    [self.scrollView addSubview:page2View];
+    
+    //第三頁
+    UIView *page3View = [[UIView alloc]initWithFrame:CGRectMake(width * 2, 0, width, height)];
+    page3View.backgroundColor = [UIColor blueColor];
+    [self.scrollView addSubview:page3View];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - implement UIScrollViewDelegate
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    //利用寬度與contentOffset.x的距離來計算當前頁面為何，來改變pageControl的顯示
+    CGFloat width = scrollView.frame.size.width;
+    NSInteger currentPage = ((scrollView.contentOffset.x - width / 2) / width) + 1;
+    [self.pageControl setCurrentPage:currentPage];
 }
 
 @end
